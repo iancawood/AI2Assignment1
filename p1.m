61f5a08ce8a4ed4bd278a43622d48450edd37820
Ian Wood <ian.wood108@gmail.com>
2017-01-28 16:50:29 -0500

    problem 1

---
+++ p1.m
@@ -0,0 +1,18 @@
+function [X_out, Y_out] = p1(X, Y, l1, l2)
+
+l1Indices = find(Y == l1);
+l2Indices = find(Y == l2);
+
+X_out = X([l1Indices; l2Indices], :);
+
+if l1 < l2
+    YL1 = ones(size(l1Indices, 1), 1);
+    YL2 = ones(size(l2Indices, 1), 1) * 2;
+else    
+    YL1 = ones(size(l1Indices, 1), 1) * 2;
+    YL2 = ones(size(l2Indices, 1), 1);
+end
+
+Y_out = [YL1; YL2];
+end
+
